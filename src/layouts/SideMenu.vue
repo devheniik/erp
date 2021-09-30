35e7a18c1eb9fc4bef267c9f06980a19600e21bc
EgamixYT <55224974+EgamixYT@users.noreply.github.com>
2021-09-30 14:44:25 +0300

    Side menu fixes

--- src/layouts/SideMenu.vue
+++ src/layouts/SideMenu.vue
@@ -84,17 +84,28 @@
             </div>
             <nav class="mt-5 flex-1" aria-label="Sidebar">
               <div class="px-2 space-y-2 ">
-                  <div v-for="(item, i) in navigation" :key="item.name" :href="i" >
-                    <div @click="handleClick(item) " :class="[item.current ? 'bg-gray-200 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 ', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-                      <component :is="item.icon" :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-4 h-6 w-6']" aria-hidden="true" />
+                  <div v-for="(item, i) in navigation" :key="item.name" :href="i" class="" >
+                    <div @click="handleClick(item) " :class="[item.current ? 'bg-gray-200 text-gray-900' : 'text-gray-600 hover:bg-gray-300 hover:text-gray-900 ', 'group flex justify-between items-center px-2 py-2 text-base font-medium rounded-md']">
+                     <div class="flex flex-wrap">
+                        <component :is="item.icon" :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500 hover:bg-gray-300', 'mr-4 h-6 w-6 ']" aria-hidden="true" />
                       {{ item.name }} 
+                     </div>
+                      <ChevronDownIcon class="h-4 w-4" v-if="item.current && item.child.length > 0" />
+                      <ChevronRightIcon class="h-4 w-4" v-else-if="item.child.length > 0"/>
+                     
                     </div>
                     <transition enter-active-class="opacity-0 -translate-y-6 h-0" leave-active-class="opacity-0 -translate-y-6">
+                      
                       <div v-if="item.current && item.child.length" class="transform duration-300 ease-out mt-2 space-y-2 ">
-                        <div  v-for="(subItem, j) in item.child" :key="j"  >
-                          <div @click="handleClick(subItem)"  :class="[subItem.current ? 'bg-gradient-to-r from-gray-500 to-gray-400 text-white shadow-2xl filter drop-shadow-lg ' : 'text-gray-600 bg-gray-200 hover:bg-gray-50 hover:text-gray-900  whitespace-nowrap truncate', 'pl-5 group flex items-center px-3 py-2 text-base font-medium rounded-md']">
-                            <component :is="subItem.icon" :class="[subItem.current ? 'text-white' : 'text-gray-400 group-hover:text-gray-500 ', 'mr-4 h-4 w-4']" aria-hidden="true" />
-                            {{ subItem.name }} 
+                        <div  v-for="(subItem, j) in item.child" :key="j" class=""  >
+                          
+                          <div @click="handleClick(subItem)"  :class="[subItem.current ? 'bg-gradient-to-r from-gray-500 to-gray-400 text-white shadow-2xl filter drop-shadow-lg ' : 'text-gray-600 bg-gray-200 hover:bg-gray-300 hover:text-gray-900  whitespace-nowrap truncate', 'pl-5 group flex  justify-between items-center px-3 py-2 text-base font-medium rounded-md']">
+                            <div class="flex flex-wrap items-center"> 
+                              <component :is="subItem.icon" :class="[subItem.current ? 'text-white' : 'text-gray-400 group-hover:text-gray-500 ', 'mr-4 h-4 w-4']" aria-hidden="true" />
+                            {{ subItem.name }}
+                             </div>
+                              <ChevronDownIcon class="h-4 w-4" v-if="subItem.current && subItem.child.length > 0" />
+                      <ChevronRightIcon class="h-4 w-4" v-else-if="subItem.child.length > 0"/>
                           </div>
                           <transition enter-active-class="opacity-0 -translate-y-6 h-0" leave-active-class="opacity-0 -translate-y-6">
                             <div v-if="subItem.current && subItem.child.length" class="transform duration-300 ease-out mt-2 space-y-2">
