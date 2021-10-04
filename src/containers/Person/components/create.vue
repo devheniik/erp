21d485050f2b33be6c3e67c33ffb5aeb33908d0e
EgamixYT <55224974+EgamixYT@users.noreply.github.com>
2021-10-04 21:34:48 +0300

    /

--- src/containers/Person/components/create.vue
+++ src/containers/Person/components/create.vue
@@ -208,60 +208,11 @@
                                     >Сотрудник:</label
                                 >
                                 <div>
-                                    <div class="relative">
-                                        <button
-                                            type="button"
-                                            class="
-                                                relative
-                                                w-full
-                                                bg-white
-                                                border border-gray-300
-                                                rounded-md
-                                                shadow-sm
-                                                pl-3
-                                                pr-10
-                                                py-2
-                                                text-left
-                                                cursor-default
-                                                focus:outline-none
-                                                focus:ring-1
-                                                focus:ring-green-500
-                                                focus:border-green-500
-                                                sm:text-sm
-                                            "
-                                            aria-haspopup="listbox"
-                                            aria-expanded="true"
-                                            aria-labelledby="listbox-label"
-                                        >
-                                            <span
-                                                class="
-                                                    block
-                                                    truncate
-                                                    text-gray-500
-                                                "
-                                                >Выбрать</span
-                                            >
-                                            <span
-                                                class="
-                                                    absolute
-                                                    inset-y-0
-                                                    right-0
-                                                    flex
-                                                    items-center
-                                                    pr-2
-                                                    pointer-events-none
-                                                "
-                                            >
-                                                <ChevronDownIcon
-                                                    class="
-                                                        h-5
-                                                        w-5
-                                                        text-gray-400
-                                                    "
-                                                />
-                                            </span>
-                                        </button>
-                                    </div>
+                                <select  class="dropdown-primary">
+                                    <option>USA</option>
+                                    <option selected="">Canada</option>
+                                    <option>EU</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="mt-2 ml-2">
