ae23e968b5c0f470fa4904b06ff503c4ecc0c01b
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-10-04 16:21:03 +0300

    fix: list

--- src/containers/Person/views/List.vue
+++ src/containers/Person/views/List.vue
@@ -9,27 +9,5 @@
 </template>
 
 <script setup>
-    import { ref, provide, watchEffect } from 'vue'
-    import Filters from '../components/filters.vue'
-    import Create from '@dashboard/components/create.vue'
-    import hooks from '../hooks/list'
-    
-
-    const {
-        data,
-        isLoad
-    } = hooks()
-
-    const newPersonRef = ref(false)
-
-    provide('ref', newPersonRef)
-    provide('refChange', () => newPersonRef.value = true) 
-
-    watchEffect(() => {
-      console.log(data.value)
-    });
- 
-
- 
     import Create from '../components/create.vue'
 </script>
