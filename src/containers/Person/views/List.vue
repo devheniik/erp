9a42663f21f9a1919d4769b52d65aec3d7e6f763
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-10-05 22:26:03 +0300

    add update

--- src/containers/Person/views/List.vue
+++ src/containers/Person/views/List.vue
@@ -5,7 +5,7 @@
                 <create></create>
             </template>
             <template v-slot:edit>
-                <create></create>
+                <update></update>
             </template>
         </uviewer>
     </div>
@@ -13,4 +13,5 @@
 
 <script setup>
     import Create from '../components/create.vue'
+    import Update from '../components/update.vue'
 </script>
