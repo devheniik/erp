08c73bfc1c2894a4cdf651dcec7f598bf5bb7911
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-10-25 20:47:20 +0300

    modal demo

--- src/containers/Dashboard/views/D2.vue
+++ src/containers/Dashboard/views/D2.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class="w-4/12">
-        <roll-up></roll-up>
+    <div> 
+        <div @click="show = true">
+        показать
+        </div>
+            <modal v-model="show" width="w-8/12">
+                <Attestation></Attestation>
+            </modal>
     </div>
 </template>
 
 <script setup>
-
+    import Attestation from '../../../components/uniprops/Persons/Adaptation/Attestation.vue'
+    import {
+        ref
+    } from 'vue'
+    const show = ref(false)
 </script>
 
 <style lang="scss" scoped>
