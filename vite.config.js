0235355a5f61c779e6650e5e9260dbd7b92a8497
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-11-05 14:46:00 +0200

    add file to gitignore

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig, loadEnv  } from 'vite'
+import pugPlugin from "vite-plugin-pug"
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const options = { pretty: true } // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" }
+ 
+
+export default ({ mode }) => { 
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    // server: {
+    //   host: "erpstorm.de",
+    //   port: 3000,
+    //   https: true,
+    // },
+    plugins: [vue(), pugPlugin(options, locals)],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '/src'),
+        '@axios': path.resolve(__dirname, '/src/plugins/axios.js'), 
+        '@api_base': path.resolve(__dirname, '/src/api/base'), 
+        '@api': path.resolve(__dirname, '/src/api/index'), 
+        'h_list': path.resolve(__dirname, '/src/hooks/list.js'),
+        '@user': path.resolve(__dirname, '/src/containers/User'),
+        '@permitions': path.resolve(__dirname, '/src/containers/Permitions'),
+        '@sale': path.resolve(__dirname, '/src/containers/Sale'),
+        '@product': path.resolve(__dirname, '/src/containers/Product'),
+        '@operation': path.resolve(__dirname, '/src/containers/Operation'),
+        '@firm': path.resolve(__dirname, '/src/containers/Firm'),
+        '@finance': path.resolve(__dirname, '/src/containers/Finance'),
+        '@person': path.resolve(__dirname, '/src/containers/Person'),
+        '@dashboard': path.resolve(__dirname, '/src/containers/Dashboard')
+      },
+    }
+  });
+}
