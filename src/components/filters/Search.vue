7a0c3924043302dc605ae633bcf49920a07dc60f
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-11-17 16:40:14 +0200

    add value_d

--- src/components/filters/Search.vue
+++ src/components/filters/Search.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div> 
-            <input :disabled="data.readonly" type="text" @input="$emit('update:modelValue', $event.target.value)" @keyup.enter="$emit('change')"  class="input-primary" :placeholder="data.label" :value="modelValue" />
+            <input :disabled="data.readonly" type="text" @input="$emit('update:modelValue', $event.target.value)" @keyup.enter="$emit('change')"  class="input-primary" :placeholder="data.label" :value="modelValue  ?? data.value_default" />
         </div>
     </div>
 </template>
