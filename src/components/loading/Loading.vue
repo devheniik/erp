bc8c66c749d8e39a3abe0cf3976329237d5c78bd
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-11-22 12:59:11 +0200

    add loading

--- src/components/loading/Loading.vue
+++ src/components/loading/Loading.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="h-full w-full top-0 left-0 z-40">
-    <div class="absolute w-full h-full bg-white  opacity-40">
-
-    </div>
-    <div class="flex items-center justify-center w-full  h-full z-40">
-      <div class="flex items-center justify-center flex-shrink-0 text-gray-700"> 
+  <div class="h-full flex items-center justify-center w-full top-0 left-0 z-40">
+    <div class="mt-4 z-40">
+      <div class="flex items-center justify-center flex-shrink-0 text-gray-800"> 
         <CogIcon class="h-16 w-16 animate-spin" />
         <div class="flex flex-col">
-          <span class="font-extrabold text-3xl text-black">EUROZOL</span>
-          <span class="font-bold text-sm -mt-2 text-black">BUILD IT</span>
+          <span class="font-extrabold text-3xl text-gray-800">EUROZOL</span>
+          <span class="font-bold text-sm -mt-2 text-gray-800">BUILD IT</span>
         </div>
       </div>
+    </div>
+    <div class="absolute w-full h-full bg-white opacity-40">
+
     </div>
   </div>
 </template>
