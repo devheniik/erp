4dc9fa97629123ef2dccab8db3c6f121a5523da2
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-02-01 14:52:55 +0200

    fix page

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -1,5 +1,5 @@
 <template> 
-        <div ref="page" class="h-full w-full flex items-center justify-center font-bold text-lg">
+        <div class="h-full w-full flex items-center justify-center font-bold text-lg">
             Открыто в новой странице
         </div> 
 </template>
